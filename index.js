71d0571653f218cc9ee24baa3b83f8ff4728cbaf
Chris <me@chrishoenack.com>
2016-09-15 19:46:01 +0000

    Add File Server

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 import http from 'http';
+import fs from 'fs';
 
 const port = process.env.PORT;
 
-function onRequest(request, response) {
-    response.writeHead(200, { 'Context-Type': 'text/plain' });
-    response.write('The server responded');
+// Handle 404
+function pageNotFound(request, response) {
+    response.writeHead(404, { 'Context-Type': 'text/plain' });
+    response.write(`404: File not found at ${request.url}`);
     response.end();
+}
+
+// Handle Request
+function onRequest(request, response) {
+    if (request.method === 'GET' && request.url === '/') {
+        response.writeHead(200, { 'Context-Type': 'text/html' });
+        fs.createReadStream('./src/index.html').pipe(response);
+    } else {
+        pageNotFound(request, response);
+    }
 }
 
 http.createServer(onRequest).listen(port);
